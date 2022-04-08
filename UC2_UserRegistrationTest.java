e7b96cfaa8e3bd7e651c2d79692a1bf01cce546a
barbadeadesh <barbadeadesh2020@email.com>
2022-04-08 18:10:56 +0530

    ADDED: Function for test the validation

---
+++ UC2_UserRegistrationTest.java
@@ -0,0 +1,56 @@
+package com.bridgelab.junittesting;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class UserRegistrationTest {
+	@Test
+	public void givenfirstnameshouldbeTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validateFirstName("Soma");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastnameshouldbeTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validateLastName("Gosavi");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void validateforemail() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validateEmailTest("abc.xyz@bl.co.in");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenMobilenoconditionmeetthenTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validateMobileNumber("91 9421551510");
+		Assert.assertEquals(true, result);
+
+	}
+
+	@Test
+	public void givenPasswordconditionmeetthenTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validatePassword("Abcdedfs");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPassword1conditionmeetthenTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validatePasswordatleastoneUpperCase("Aberdsds");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPassword2conditionmeetthenTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validatePasswordatleastonenumericNumber("A9asfghg");
+		Assert.assertEquals(true, result);
+	}
+}
